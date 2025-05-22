c3ca1903c241d5d9888d0bd0c848bd0f7160c478
Benjamin <benjaminurrea14@gmail.com>
2025-05-22 13:32:58 -0400

    guardado

--- src/main/java/org/example/DatosLogin.java
+++ src/main/java/org/example/DatosLogin.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DatosLogin {
+    public boolean existeUsuario(String usuario, String contraseña){
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader("src/main/resources/login.txt"));
+            String linea;
+
+            while ((linea = lector.readLine()) != null) {
+                String [] partes = linea.split(",");
+
+            if (partes.length == 2 && partes[0].equals(usuario) && partes [1],equals(contraseña)) {
+                return true;
+                }
+            }
+        }
+    }
 }
