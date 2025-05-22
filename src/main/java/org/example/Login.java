a62d29ea3f85111acbf5e85b545b9b06fa2d9b3b
Benjamin <benjaminurrea14@gmail.com>
2025-05-22 14:27:46 -0400

    Cambio de codigo (errores recurrentes)

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -7,9 +7,6 @@ public class Login {
         datos = new DatosLogin();
     }
 
-    /**
-     * Verifica usuario y contraseña usando DatosLogin.
-     */
     public boolean autenticar(String usuario, String contraseña) {
         return datos.verificarCredenciales(usuario, contraseña);
     }
