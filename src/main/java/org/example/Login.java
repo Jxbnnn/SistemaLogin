3b69a92e1fba0d24acef8507ac97cc400904b1b7
Benjamin <benjaminurrea14@gmail.com>
2025-05-22 13:24:58 -0400

    Datos de login

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Login {
+    private DatosLogin datos;
+
+    public Login() {
+        this.datos = new DatosLogin();
+    }
+
+    public boolean verificar(String ususuario, String contraseña) {
+        return datos.existeUsuario(usuario, contraseña);
+    }
 }
