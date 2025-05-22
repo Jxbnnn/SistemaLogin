9ade6770ccc8706a4b632b7dbeb2b3992d9b667c
Benjamin <benjaminurrea14@gmail.com>
2025-05-22 13:13:49 -0400

    Login inicio de sesion

--- src/main/java/org/example/ConsolaLogin.java
+++ src/main/java/org/example/ConsolaLogin.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class ConsolaLogin {
+    private Login login;
+
+    public ConsolaLogin() {
+        this.login = new Login ();
+        public void mostrarMenu() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("----------------------------------");
+            System.out.println("    Iniciando sistema de login    ");
+            System.out.println("----------------------------------");
+            System.out.println("Usuario: ");
+            String usuario = scanner.nextLine();
+            System.out.println("Contraseña: ");
+            String contraseña = scanner.nextLine();
+        }
+    }
 }
