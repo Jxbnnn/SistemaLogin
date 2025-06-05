07495850016e809c5f0f8bd7c21c936f9125058c
Benjamin <benjaminurrea14@gmail.com>
2025-06-05 10:34:30 -0400

    Datos sesion (terminado)

--- src/main/java/Modelo/GestorUsuarios.java
+++ src/main/java/Modelo/GestorUsuarios.java
@@ -1,4 +1,28 @@
 package Modelo;
 
+import java.io.*;
+
 public class GestorUsuarios {
+
+    public GestorUsuarios() {
+        File archivo = new File("login.txt");
+
+        if (!archivo.exists()) {
+            try {
+                archivo.createNewFile();
+            } catch (IOException e) {
+                System.out.println("No se pudo crear login.txt");
+            }
+        }
+    }
+
+    public boolean registrar(String nombre, String clave) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("login.txt", true))) {
+            bw.write(nombre + ";" + clave);
+            bw.newLine();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 }
