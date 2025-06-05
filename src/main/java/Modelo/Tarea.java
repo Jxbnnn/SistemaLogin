2804ff6df2f1fe87f9cb43529dcd7ae52351f317
Benjamin <benjaminurrea14@gmail.com>
2025-06-05 09:48:48 -0400

    commit tarea

--- src/main/java/Modelo/Tarea.java
+++ src/main/java/Modelo/Tarea.java
@@ -1,4 +1,13 @@
 package Modelo;
 
 public class Tarea {
+    private String descripcion;
+
+    public Tarea(String descripcion){
+        this.descripcion = descripcion;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+
 }
