887ff50db3d619e37206d6af851a64c91d01669d
Benjamin <benjaminurrea14@gmail.com>
2025-06-05 10:22:15 -0400

    Datos sesion (terminado)

--- src/main/java/Modelo/DatosSesion.java
+++ src/main/java/Modelo/DatosSesion.java
@@ -7,24 +7,41 @@ public class DatosSesion {
     private final String archivo;
     private final ArrayList<Tarea> tareas = new ArrayList<>();
 
-    public DatosSesion (String usuario) {
+    public DatosSesion(String usuario) {
         this.archivo = usuario + "_todo.txt";
-        File file = new File (archivo);
+        File file = new File(archivo);
 
         if (!file.exists()) {
             try {
-                file.createNewFile();{
-            } catch (IOException e){
-                    System.out.println("No se pudo crear el archivo de tareas.");
-                }
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("No se pudo crear el archivo de tareas.");
+            }
         }
-    }
-        try (BufferedReader br = new BufferedReader(new FileReader(file))){
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String linea;
-            while ((linea = br.readLine() !=null){
+            while ((linea = br.readLine()) != null) {
                 tareas.add(new Tarea(linea));
             }
-        }catch (IOException e) {
-            System.out.println("Error al leer el archivo de tareas");
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo de tareas.");
         }
+    }
+
+    public void agregarTarea(String descripcion) {
+        Tarea nueva = new Tarea(descripcion);
+        tareas.add(nueva);
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))) {
+            bw.write(descripcion);
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println("No se pudo guardar la tarea.");
+        }
+    }
+
+    public ArrayList<Tarea> getTareas() {
+        return tareas;
+    }
 }
