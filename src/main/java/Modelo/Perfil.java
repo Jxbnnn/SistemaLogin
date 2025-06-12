a673bf87dab09c1428d5c395bd99389bf81b3d44
Benjamin <benjaminurrea14@gmail.com>
2025-06-12 13:25:42 -0400

    commit 12 junio

--- src/main/java/Modelo/Perfil.java
+++ src/main/java/Modelo/Perfil.java
@@ -1,4 +1,20 @@
 package Modelo;
+import java.time.LocalDateTime;
 
 public class Perfil {
+    private String correo;
+    private LocalDataTime fechaCreacion;
+
+    public Perfil (String correo){
+        this.correo = correo;
+        this.fechaCreacion = LocalDateTime.now();
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public LocalDateTime getFechaCreacion(){
+        return fechaCreacion;
+    }
 }
