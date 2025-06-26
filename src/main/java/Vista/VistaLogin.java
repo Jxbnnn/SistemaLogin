81797129f2859aaebcf89a0dc4ee42c9c7e3c940
Benjamin <benjaminurrea14@gmail.com>
2025-06-26 13:33:31 -0400

    commit 26 junio

---
+++ src/main/java/Vista/VistaLogin.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import Controlador.Login;
+import Modelo.DatosLogin;
+import Modelo.GestorUsuarios;
+import Modelo.Usuario;
+import Modelo.DatosSesion;
+import javax.swing.*;
+import java.awt.*;
+
+public class VistaLogin {
+}
