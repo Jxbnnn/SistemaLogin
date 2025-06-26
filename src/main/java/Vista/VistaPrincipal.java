1d7970ca973cc719a533b9cf1dba7468d499956c
Benjamin <benjaminurrea14@gmail.com>
2025-06-26 13:59:34 -0400

    commit 26 junio VistaPrincipal

--- src/main/java/Vista/VistaPrincipal.java
+++ src/main/java/Vista/VistaPrincipal.java
@@ -14,4 +14,17 @@ public class VistaPrincipal extends JFrame {
     private JList<String> listaTareas;
     private JTextField campoBusqueda;
 
+    public VistaPrincipal (DatosSesion datosSesion) {
+        this.usuario = datosSesion.getUsuario();
+        this.datosSesion = datosSesion;
+
+        setTitle("Tareas de " + usuario.getNombreUsuario());
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        inicializarComponentes();
+        cargarTareas();
+    }
+
 }
