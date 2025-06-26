72475a3a05165483e5fedb1747350901c25a1fa3
Benjamin <benjaminurrea14@gmail.com>
2025-06-26 13:56:05 -0400

    commit 26 junio VistaPrincipal

--- src/main/java/Vista/VistaPrincipal.java
+++ src/main/java/Vista/VistaPrincipal.java
@@ -1,4 +1,15 @@
 package Vista;
 
-public class VistaPrincipal {
+import Modelo.*;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+
+public class VistaPrincipal extends JFrame {
+    private final Usuario usuario;
+    private final DatosSesion datosSesion;
+    private DefaultListModel<String> modeloTareas;
+
 }
